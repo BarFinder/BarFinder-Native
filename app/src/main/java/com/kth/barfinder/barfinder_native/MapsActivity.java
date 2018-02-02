179afb5492303acecb68a8c346dbebb82ad4b64d
Shayan <shayanf@kth.se>
2018-02-02 11:38:38 +0100

    location service

--- app/src/main/java/com/kth/barfinder/barfinder_native/MapsActivity.java
+++ app/src/main/java/com/kth/barfinder/barfinder_native/MapsActivity.java
@@ -3,7 +3,9 @@ package com.kth.barfinder.barfinder_native;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -19,6 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +35,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                       // mMap.setOnMyLocationButtonClickListener(this);
+                       // mMap.setOnMyLocationClickListener(this);
+                    }
+                } else {
+                    LatLng stockholm = new LatLng(40.74, -74) ;
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm));
+                }
+                break;
+        }
+    }
+
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Stockholm and move the camera
-        LatLng stockholm = new LatLng(59.33, 18.06);
-        mMap.addMarker(new MarkerOptions().position(stockholm).title("Marker in Stockholm"));
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        LatLng stockholm = new LatLng(40.74, -74);
+        //  mMap.addMarker(new MarkerOptions().position(stockholm).title("Marker in Stockholm"));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
-            mMap.setOnMyLocationButtonClickListener(this);
-            mMap.setOnMyLocationClickListener(this);
+            //  mMap.setOnMyLocationButtonClickListener(this);
+          //  mMap.setOnMyLocationClickListener(this);
         } else {
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm));
+            // mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]
+                        {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
         }
     }
 
     @Override
-    public void onMyLocationClick(@NonNull Location location){
+    public void onMyLocationClick(@NonNull Location location) {
         Toast.makeText(this, "Current Location:\n" + location, Toast.LENGTH_LONG).show();
     }
 
