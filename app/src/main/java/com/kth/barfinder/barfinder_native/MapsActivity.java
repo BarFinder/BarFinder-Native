b0b381769abfb3d618d98a4def0c5ec1db6c35bc
Shayan <shayanf@kth.se>
2018-02-07 13:20:18 +0100

    Move location button

--- app/src/main/java/com/kth/barfinder/barfinder_native/MapsActivity.java
+++ app/src/main/java/com/kth/barfinder/barfinder_native/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.kth.barfinder.barfinder_native;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
@@ -10,7 +11,9 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
@@ -42,7 +45,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /*
         Constantly checks if anything has been edited inside the input box
      */
-    public void ConstInputListener(){
+    public void ConstInputListener() {
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
@@ -62,6 +65,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    /*
+        Moves the location button to the bottom right
+     */
+    @SuppressLint("ResourceType")
+    public void MoveLocationButton() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().
+                findFragmentById(R.id.map);
+        View mapView = mapFragment.getView();
+        if (mapView != null &&
+                mapView.findViewById(1) != null) {
+            // Get the button view
+            View locationButton = ((View) mapView.findViewById(1).getParent()).findViewById(2);
+            // and next place it, on bottom right (as Google Maps app)
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationButton.getLayoutParams();
+            // position on right bottom
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 30);
+        }
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -73,6 +98,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         mMap.setMyLocationEnabled(true);
                         mMap.setOnMyLocationButtonClickListener(this);
                         mMap.setOnMyLocationClickListener(this);
+                        MoveLocationButton();
                     }
                 } else {
                     LatLng stockholm = new LatLng(40.74, -74);
@@ -89,11 +115,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Stockholm and move the camera
         LatLng stockholm = new LatLng(59.334591, 18.063240);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm));
+
         //  mMap.addMarker(new MarkerOptions().position(stockholm).title("Marker in Stockholm"));
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
             mMap.setOnMyLocationButtonClickListener(this);
             mMap.setOnMyLocationClickListener(this);
+            MoveLocationButton();
         } else {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]
